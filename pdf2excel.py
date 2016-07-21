75c72e5d250a04a97279fed8d5d65952ae0d6248
AlexSunChen <sunchen1221@gmail.com>
2016-07-22 07:37:46 +0900

    add the export excel and pdftotxt

--- pdf2excel.py
+++ pdf2excel.py
@@ -1,4 +1,8 @@
+# -*- coding: UTF-8 -*-
+
 import Tkinter, Tkconstants, tkFileDialog
+import xlwt
+import os
 
 class ParserPdfProcess(Tkinter.Frame):
 
@@ -13,7 +17,7 @@ class ParserPdfProcess(Tkinter.Frame):
     Tkinter.Button(self, text='Select the PDF Folder', command=self.askdirectory).pack(**button_opt)
     Tkinter.Label(self, textvariable=self.FolderName).pack()
     self.FolderName.set("Please Choose a directory")
-    Tkinter.Button(self,text="Export Excel", command=self.PdfToExcel).pack(**button_opt)
+    Tkinter.Button(self,text="Export Excel", command=self.ExportExcel).pack(**button_opt)
 
     # defining options for opening a directory
     self.dir_opt = options = {}
@@ -28,9 +32,46 @@ class ParserPdfProcess(Tkinter.Frame):
     self.FolderName.set(tkFileDialog.askdirectory(**self.dir_opt))
     return
 
-  def PdfToExcel(self):
-      self.PdfToTxt()
-      list = self.ParserListWord()
+  def ExportExcel(self):
+    ParserPdfProcess.PdfToTxt(self)
+    lists = ParserPdfProcess.ParserListWord(self)
+    dicts = {'橋本': '1213', '長じ': '121', 'カツミ': '3258'}
+    book = xlwt.Workbook(encoding='utf-8', style_compression=0)
+    sheet = book.add_sheet('MN', cell_overwrite_ok=True)
+
+    i = 0
+    j = 0
+    for key, value in dicts.items():
+      sheet.write(i, j, key.decode('utf-8'))
+      sheet.write(i, j+1, value)
+      i += 1
+    book.save(self.FolderName.get() + '/export.xls')
+
+    self.FolderName.set("Completely Export the data !")
+
+  def ParserListWord(self):
+    fileList = os.listdir(self.FolderName.get())
+    os.chdir(self.FolderName.get())
+    for file in fileList:
+      if ".txt" in file:
+        curFile = open(file, mode='r')
+        while 1:
+          line = curFile.readline()
+          line = line.replace(' ','')
+          keyword = 'ニフティ株式会社'
+          if keyword in line:
+            print line
+          if not line:
+            break
+    return
+
+  def PdfToTxt(self):
+    fileList = os.listdir(self.FolderName.get())
+    os.chdir(self.FolderName.get())
+    for file in fileList:
+      if ".pdf" in file:
+        os.system('pdf2txt.py  -o ' + file[0:-4] + '.txt ' + file)
+    return
 
 
 if __name__=='__main__':
